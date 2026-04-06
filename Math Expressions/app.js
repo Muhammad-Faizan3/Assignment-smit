f97d79708b7a33576ff49e646c2c56d829311193
Muhammad Faizan <fb240951@gmail.com>
2026-04-06 20:15:00 +0500

    MAth Expressions

---
+++ Math Expressions/app.js
@@ -0,0 +1,38 @@
+// // 1. Addition
+// let a = 5;
+// let b = 3;
+// console.log(a + b);
+
+// // 2. Subtraction
+// console.log(a - b);
+
+// // 3. Multiplication
+// console.log(a * b);
+
+// // 4. Division
+// console.log(a / b);
+
+// // 5. Modulus
+// console.log(a % b);
+
+// // 6. Increment
+// let num = 10;
+// num++;
+// console.log(num);
+
+// // 7. Decrement
+// num--;
+// console.log(num);
+
+// // 8. Combined operation
+// let result = (5 + 3) * 2;
+// console.log(result);
+
+// // 9. Ticket cost
+// let ticket = 600;
+// console.log(ticket * 5);
+
+// // 10. Percentage
+// let obtained = 400;
+// let total = 500;
+// console.log((obtained / total) * 100 + "%");
