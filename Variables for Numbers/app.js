8309ce8c9056b9cbafa15fe0ca54d4144b0b7912
Muhammad Faizan <fb240951@gmail.com>
2026-04-06 20:24:18 +0500

    Variables of Numbers

---
+++ Variables for Numbers/app.js
@@ -0,0 +1,53 @@
+// 1. Declare a variable called age & assign your age
+let age = 20;
+console.log(age);
+
+
+// 2. Declare & add two numbers
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+console.log(sum);
+
+
+// 3. Subtract two numbers
+let sub = num1 - num2;
+console.log(sub);
+
+
+// 4. Multiply two numbers
+let mul = num1 * num2;
+console.log(mul);
+
+
+// 5. Divide two numbers
+let div = num1 / num2;
+console.log(div);
+
+
+// 6. Increment a number
+let a = 5;
+a++;
+console.log(a);
+
+
+// 7. Decrement a number
+let b = 5;
+b--;
+console.log(b);
+
+
+// 8. Store total marks & obtained marks
+let totalMarks = 500;
+let obtainedMarks = 400;
+console.log(totalMarks, obtainedMarks);
+
+
+// 9. Calculate percentage
+let percentage = (obtainedMarks / totalMarks) * 100;
+console.log(percentage);
+
+
+// 10. Simple calculation
+let result = (10 + 5) * 2;
+console.log(result);
