9af5c1e46305868ccebb66cfe27a98d3ceee3f11
Muhammad Faizan <fb240951@gmail.com>
2026-04-06 20:18:49 +0500

    Variable Name (legal & lllegal)

---
+++ Variable Names (Legal & Illegal)/app.js
@@ -0,0 +1,26 @@
+// 1. Declare 3 variables in one statement
+let name = "Faizan", age = 20, city = "Karachi";
+
+
+// 2. Legal Variable Names
+let userName;
+let $price;
+let _total;
+let myVariable1;
+let first_name;
+
+
+// 3. Illegal Variable Names
+// let 1name;
+// let my-name;
+// let var;
+// let user name;
+// let @value;
+
+
+// 4. Display Rules
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, numbers, $ and _. <br>");
+document.write("Variables must begin with a letter, $ or _. <br>");
+document.write("Variable names are case sensitive. <br>");
+document.write("Variable names should not be JS keywords.");
