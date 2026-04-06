c6a9e52d2792ef0376ae46580a566a217b6d03bb
Muhammad Faizan <fb240951@gmail.com>
2026-04-06 20:28:41 +0500

    Variable for string

---
+++ Variables for Strings/app.js
@@ -0,0 +1,49 @@
+// 1. Declare a string variable
+let name = "Faizan";
+console.log(name);
+
+
+// 2. Declare and print message
+let message = "Hello World";
+console.log(message);
+
+
+// 3. Combine first and last name
+let firstName = "Faizan";
+let lastName = "Bashir";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+
+// 4. Store favorite color
+let color = "Blue";
+console.log("My favorite color is " + color);
+
+
+// 5. Store city name
+let city = "Karachi";
+console.log("I live in " + city);
+
+
+// 6. Create greeting message
+let greet = "Welcome " + name;
+console.log(greet);
+
+
+// 7. Store email
+let email = "faizan@example.com";
+console.log("My email is " + email);
+
+
+// 8. Store phone number as string
+let phone = "03001234567";
+console.log(phone);
+
+
+// 9. Create sentence using multiple strings
+let sentence = name + " lives in " + city;
+console.log(sentence);
+
+
+// 10. Alert message
+alert("Hello " + name);
