5f00874eb9c50c733a402b7dcbdd775d5d97b261
Muhammad Faizan <fb240951@gmail.com>
2026-04-06 20:10:30 +0500

    Concatenating Strings, IF Statements, Comparison Operators

---
+++ Concatenating Strings, IF Statements, Comparison Operators/app.js
@@ -0,0 +1,44 @@
+// let firstName = "Faizan";
+// let lastName = "Bashir";
+
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+// let msg = "Welcome " + fullName;
+// console.log(msg);
+
+// let age = 18;
+
+// if (age >= 18) {
+//     console.log("Adult");
+// } else {
+//     console.log("Not Adult");
+// }
+
+// let marks = 75;
+
+// if (marks >= 50) {
+//     console.log("Pass");
+// } else {
+//     console.log("Fail");
+// }
+
+
+// let a = 10;
+// let b = 5;
+
+// console.log(a == b);   // false
+// console.log(a != b);   // true
+// console.log(a > b);    // true
+// console.log(a < b);    // false
+// console.log(a >= b);   // true
+// console.log(a <= b);   // false
+
+// let name = "Faizan";
+// let age2 = 20;
+
+// if (age2 >= 18) {
+//     console.log("Hello " + name + " you can vote");
+// } else {
+//     console.log("Hello " + name + " you cannot vote");
+// }
